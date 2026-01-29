e0ecacc3e668cdc53667d697844be9ed459fd2d8
itsnishant4 <itsnishant470@gmail.com>
2026-01-29 10:09:49 +0530

    style: Apply Inter font to the body using `next/font/google`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
@@ -155,6 +161,7 @@ export default function RootLayout({
       <body
         className={cn(
           "antialiased bg-background!   min-h-screen max-w-2xl mx-auto py-12 sm:py-24 px-6",
+          fontSans.variable
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="light">
